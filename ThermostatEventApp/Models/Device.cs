cc44f4bc1604f753f645a2127d1e96bc03bd47ef
austinhimler <usclaxx10@gmail.com>
2023-11-20 13:55:31 -0500

    start implementing classes based on interfaces

---
+++ ThermostatEventApp/Models/Device.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThermostatEventApp.Interfaces;
+
+namespace ThermostatEventApp.Models
+{
+    public class Device : IDevice
+    {
+        const double Warning_Level = 27;
+        const double Emergency_Level = 75;
+        public double WarningTemperature => Warning_Level;
+
+        public double EmergencyTemperature => Emergency_Level;
+
+        public void HandleEmergency()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Sending out notifications to emergency services personal...");
+            
+        }
+
+        private void ShutDownDevice()
+        {
+            Console.WriteLine("Shutting down device...");
+        }
+
+        public void RunDevice()
+        {
+            Console.WriteLine("Device is running");
+
+            //ICoolingMechanism coolingMechanism = new CoolingMechanism();
+            //IHeatSensor heatSensor = new HeatSensor(Warning_Level, Emergency_Level);
+            //IThermostat heatSensor = new HeatSensor(this, heatSensor, coolingMechanism);
+
+            //thermostat.RunThermostat();
+
+        }
+    }
+}
