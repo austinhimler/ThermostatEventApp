65f40cb436026bc6cb798d3629f180f5e9d31c3d
austinhimler <usclaxx10@gmail.com>
2023-11-20 13:36:45 -0500

    Add project files.

---
+++ ThermostatEventApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ThermostatEventApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press Any Key to start device...");
+            Console.ReadKey();
+
+            //run device
+
+            Console.ReadKey();
+        }
+    }
+
+    
+}
