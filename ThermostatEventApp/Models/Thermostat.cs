9832d3dcdca5f07718804c318b0dee667c451e3e
austinhimler <usclaxx10@gmail.com>
2023-11-20 14:30:20 -0500

    i think everything is implemented, now to call

--- ThermostatEventApp/Models/Thermostat.cs
+++ ThermostatEventApp/Models/Thermostat.cs
@@ -21,7 +21,43 @@ namespace ThermostatEventApp.Models
         }
         public void RunThermostat()
         {
-            throw new NotImplementedException();
+            Console.WriteLine("Thermostat is running...");
+            WireUpEventsToEventHandlers();
+            _heatSensor.RunHeatSensor();
+        }
+
+        private void WireUpEventsToEventHandlers()
+        {
+            _heatSensor.TempWarningEventHandler += HeatSensor_TemperatureReachesWarningLevelEventHandler;
+            _heatSensor.TempBelowWarningEventHandler += HeatSensor_TemperatureFallsBelowWarningLevelEventHandler;
+            _heatSensor.TempEmergencyEventHandler += HeatSensor_TemperatureReachesEmergencyLevelEventHandler;
+        }
+
+        private void HeatSensor_TemperatureReachesEmergencyLevelEventHandler(object sender, TemperatureEventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine();
+            Console.WriteLine($"Emergency Alert!! (Emergency level is {_device.EmergencyTemperature} and above)");
+            _device.HandleEmergency();
+            Console.ResetColor();
+        }
+
+        private void HeatSensor_TemperatureFallsBelowWarningLevelEventHandler(object sender, TemperatureEventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine();
+            Console.WriteLine($"Information Alert!! Temperature falls below warning level (Warning level is between {_device.WarningTemperature} and {_device.EmergencyTemperature})");
+            _coolingMechanism.Off();
+            Console.ResetColor();
+        }
+
+        private void HeatSensor_TemperatureReachesWarningLevelEventHandler(object sender, TemperatureEventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine();
+            Console.WriteLine($"Warning ALert!! (Warning level is between {_device.WarningTemperature} and {_device.EmergencyTemperature})");
+            _coolingMechanism.On();
+            Console.ResetColor();
         }
     }
 }
